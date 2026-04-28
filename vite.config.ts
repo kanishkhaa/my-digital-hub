8a0cffefc02d8668d60edb224fa142a2633c08bc
kanishkhaa <kanishkhaams@gmail.com>
2026-04-28 11:40:01 +0530

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  cloudflare: false,
   tanstackStart: {
     server: {
-      preset: "vercel",
-    },
-  },
+      preset: "static"
+    }
+  }
 });
